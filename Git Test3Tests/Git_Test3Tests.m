80367863c89a37e10ccf7fa4e5e85c3c76bd0532
Morytala <theBest@Morytalas-Mac.local>
2014-12-15 13:08:11 -0800

    Initial Commit

---
+++ Git Test3Tests/Git_Test3Tests.m
@@ -0,0 +1,34 @@
+//
+//  Git_Test3Tests.m
+//  Git Test3Tests
+//
+//  Created by Morytala on 12/15/14.
+//  Copyright (c) 2014 Morytala. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Git_Test3Tests : XCTestCase
+
+@end
+
+@implementation Git_Test3Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
