86ab841fc877aa90e38359da9df3305cb7488102
Morytala <theBest@Morytalas-Mac.local>
2014-12-15 13:33:33 -0800

    added a test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test3
+//
+//  Created by Morytala on 12/15/14.
+//  Copyright (c) 2014 Morytala. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
